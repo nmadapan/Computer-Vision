dfbafad4099dca8e5d30501301e71badb6532396
Naveen Madapana <naveen.madapana@gmail.com>
2018-06-18 23:59:33 -0400

    Now helpers.py contains all the functions needed for homework2. main just need to call functions. Both applying the patch to the image and transforming an image using homography are done.

--- Homework_2/helpers.py
+++ Homework_2/helpers.py
@@ -22,6 +22,39 @@ def find_homography_2d(pts1, pts2):
     Hinv = np.linalg.inv(H)
     return H, Hinv / Hinv[-1,-1]
 
+def apply_homography(img_path, H):
+	H[:,2] = 1.0 # To ensure there is no translation of the image.
+	img = cv2.imread(img_path)
+	xv, yv = np.meshgrid(range(img.shape[1]), range(img.shape[0]))
+	img_pts = np.array([xv.flatten(), yv.flatten()]).T
+	trans_img_pts = np.dot(H, real_to_homo(img_pts).T)
+	trans_img_pts = homo_to_real(trans_img_pts.T).astype(int)
+	W_new, H_new = np.max(trans_img_pts[:,0]), np.max(trans_img_pts[:,1])
+	img_new = np.zeros((H_new+1, W_new+1, 3), dtype = np.uint8)
+
+	img_new[trans_img_pts[:,1].tolist(), trans_img_pts[:,0].tolist(), :] = img[img_pts[:,1].tolist(), img_pts[:,0].tolist(), :]
+
+	write_filepath = os.path.join(os.path.dirname(img_path), os.path.basename(img_path).split('.')[0]+'_new2.jpg')
+	cv2.imwrite(write_filepath, img_new)
+
+def apply_trans_patch(base_img_path, template_img_path, H):
+	## Read images
+	base_img = cv2.imread(base_img_path)
+	temp_img = cv2.imread(template_img_path)
+
+	xv, yv = np.meshgrid(range(temp_img.shape[1]), range(temp_img.shape[0]))
+	temp_img_pts = np.array([xv.flatten(), yv.flatten()]).T
+	trans_temp_img_pts = np.dot(H, real_to_homo(temp_img_pts).T)
+
+	trans_temp_img_pts = homo_to_real(trans_temp_img_pts.T).astype(int)
+	trans_temp_img_pts[:,0] = np.clip(trans_temp_img_pts[:,0], 0, base_img.shape[1])
+	trans_temp_img_pts[:,1] = np.clip(trans_temp_img_pts[:,1], 0, base_img.shape[0])
+
+	base_img[trans_temp_img_pts[:,1].tolist(), trans_temp_img_pts[:,0].tolist(), :] = temp_img[temp_img_pts[:,1].tolist(), temp_img_pts[:,0].tolist(), :]
+
+	write_filepath = os.path.join(os.path.dirname(base_img_path), os.path.basename(base_img_path).split('.')[0]+'_new.jpg')
+	cv2.imwrite(write_filepath, base_img)
+
 def real_to_homo(pts):
     # pts is a 2D numpy array of size _ x 2/3
     # This function converts it into _ x 3/4 by appending 1
@@ -41,7 +74,7 @@ def save_mps(event, x, y, flags, param):
         print(int(fac*x), int(fac*y))
 
 def create_matching_points(img_path):
-    npz_path = img_path[:-4]+'.npz'
+    npz_path = img_path[:-4] + '.npz'
     flag = os.path.isfile(npz_path)
     if(not flag):
         img = cv2.imread(img_path)
@@ -52,5 +85,5 @@ def create_matching_points(img_path):
         cv2.setMouseCallback('TempImage', save_mps, param=(fac, mps))
         cv2.imshow('TempImage', resz_img)
         cv2.waitKey(0)
-        np.savez(npz_path, mps = np.array(mps), size_info = {'x_max': img.shape[1], 'y_max': img.shape[0]})
+        np.savez(npz_path, mps = np.array(mps))
     return np.load(npz_path)
--- Homework_2/main.py
+++ Homework_2/main.py
@@ -5,37 +5,17 @@ from helpers import *
 ## Initialization
 all_img_paths = ['.\\images\\1.jpg', '.\\images\\2.jpg', '.\\images\\3.jpg', '.\\images\\Seinfeld.jpg']
 
-img_path = '.\\images\\n1.jpg'
-img_path_ref = '.\\images\\n2.jpg'
+# img_path = '.\\images\\n1.jpg'
+# img_path_ref = '.\\images\\n2.jpg'
 
-param = {}
+img_path = all_img_paths[0]
+img_path_ref = all_img_paths[-1]
 
-img_info = create_matching_points(img_path)
-pts = img_info['mps']
-param['img1'] = img_info['size_info'].item()
-
-img_ref_info = create_matching_points(img_path_ref)
-pts_ref = img_ref_info['mps']
-param['img2'] = img_ref_info['size_info'].item()
+pts = create_matching_points(img_path)['mps']
+pts_ref = create_matching_points(img_path_ref)['mps']
 
 ## Find homography from 2 --> 1
-H, Hinv = find_homography_2d(pts, pts_ref)
+H, _ = find_homography_2d(pts, pts_ref)
 
-## Transfrom points in image 2 to image 1 and update image 1
-xv, yv = np.meshgrid(range(param['img2']['x_max']), range(param['img2']['y_max']))
-img2_pts = np.array([xv.flatten(), yv.flatten()]).T
-trans_img2_pts = np.dot(H, real_to_homo(img2_pts).T) # Transform using homography
-
-trans_img2_pts = homo_to_real(trans_img2_pts.T).astype(int)
-trans_img2_pts[:,0] = np.clip(trans_img2_pts[:,0], 0, param['img1']['x_max'])
-trans_img2_pts[:,1] = np.clip(trans_img2_pts[:,1], 0, param['img1']['y_max'])
-
-## Read images
-img1 = cv2.imread(img_path)
-img2 = cv2.imread(img_path_ref)
-
-img1[trans_img2_pts[:,1].tolist(), trans_img2_pts[:,0].tolist(), :] = img2[img2_pts[:,1].tolist(), img2_pts[:,0].tolist(), :]
-
-write_filepath = os.path.join(os.path.dirname(img_path), os.path.basename(img_path)[:-4]+'_new.jpg')
-cv2.imwrite(write_filepath, img1)
-
+# apply_trans_patch(img_path, img_path_ref, H)
+apply_homography(img_path_ref, H)
