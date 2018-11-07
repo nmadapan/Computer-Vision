4b75b0c44a6382a304209a771f6385d29bcc22fd
Naveen Madapana <naveen.madapana@gmail.com>
2018-11-07 00:08:21 -0500

    homework 8 modified

---
+++ Homework_8/helpers.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def dist_mat_vec(M, vec, weights = None, method = 'ssd'):
+    # Compute distance between each row of 'M' with 'vec'
+    # method: 'ncc', 'dot', 'ssd'
+    # M : ndarray ( _ x k); vec: (1 x k)
+    # Returns a 1D numpy array of distances.
+    if(method.lower() == 'ssd'):
+        return np.linalg.norm((M - vec)*weights, axis = 1)
+    else:
+        return None
+
+def dist_mat_mat(M1, M2, weights = None, method = 'ssd'):
+    # M1, M2 --> ndarray (y1 x k) and (y2 x k)
+    # Returns y1 x y2 ndarray with the distances.
+    # If y1 and y2 are huge, it might run into MemoryError
+    D = np.zeros((M1.shape[0], M2.shape[0]))
+    for idx2 in range(M2.shape[0]):
+        D[:, idx2] = dist_mat_vec(M1, M2[idx2, :], weights = weights, method = method)
+    return D
--- Homework_8/hw8.py
+++ Homework_8/hw8.py
@@ -2,39 +2,80 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from scipy.spatial.distance import pdist
+import sys, os, time
+from helpers import *
+import glob
 
-img = cv2.imread(r'Data\Dataset1\Pic_1.jpg',0)
+def find_checkerboard_edges(img_path, dist_thresh = 0.02, display = False):
+	'''
+	Description:
+		Return checker board edges
+	Input arguments:
+		img_path: Absolute path to the image of a checker board pattern
+	Return:
+		Return lines of checker board pattern in the form of rho and theta
+	'''
 
-edges = cv2.Canny(img,100,200)
+	if(not os.path.isfile(img_path)):
+		raise IOError('ERROR! ' + img_path + ' does NOT exists !!')
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+	## Parameters
+	# dist_thresh = 0.02
 
-# plt.show()
+	img = cv2.imread(img_path, 0) # Read image as grayscale
+	height, width = img.shape
+	diag_length = np.linalg.norm([width, height])
 
-lines = cv2.HoughLines(edges,0.8,np.pi/180,40)
-print lines.shape
+	## Apply Canny edge detector
+	edges = cv2.Canny(img, 100, 200)
 
-lines = np.reshape(lines, (lines.shape[0], lines.shape[2]))
+	## Apply Hough transform to detect the edges
+	lines = cv2.HoughLines(edges, 1, np.pi/180, 40) # (_ x 1 x 2)
+	lines = np.reshape(lines, (lines.shape[0], lines.shape[2])) # (_ x 2)
 
-M = pdist(lines, metric = 'euclidean')
-print np.min(M), np.max(M)
+	## Remove the lines that are very close to each other
+	M = dist_mat_mat(lines, lines, weights = [1/diag_length, 1/np.pi])
+	flags = np.zeros(lines.shape[0]).astype(int) == 0
+	for idx, row in enumerate(M):
+		plt.bar(range(len(row)), row)
+		plt.show()
+		row[:idx+1] = 1000
+		nz_ids = np.nonzero(row < dist_thresh)[0]
+		flags[nz_ids] = False
+	lines = lines[flags]
 
-for line in lines:
-    rho, theta = line[0], line[1]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
+	if(display):
+		for line in lines:
+			rho, theta = line[0], line[1]
+			a = np.cos(theta)
+			b = np.sin(theta)
+			x0 = a*rho
+			y0 = b*rho
+			x1 = int(x0 + 1000*(-b))
+			y1 = int(y0 + 1000*(a))
+			x2 = int(x0 - 1000*(-b))
+			y2 = int(y0 - 1000*(a))
+			cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+		cv2.imshow(img_path, img)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
 
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+	return lines
 
-cv2.imshow('Lines', img)
+base_dir = r'Data\Dataset1'
 
-cv2.waitKey(0)
+img_paths = glob.glob(os.path.join(base_dir, '*.jpg'))
+
+for img_path in img_paths:
+	dist_thresh = 0.02
+	while(True):
+		lines = find_checkerboard_edges(img_path, dist_thresh = dist_thresh, display = True)
+		if(lines.shape[0] >= 22):
+			print lines.shape, 'Reducing Refining ...'
+			dist_thresh = dist_thresh * 1.15
+		elif(lines.shape[0] < 18):
+			print lines.shape, 'Increasing Refining ...'
+			dist_thresh = dist_thresh * 0.90
+		else:
+			print 'REFINED !!!'
+			break
